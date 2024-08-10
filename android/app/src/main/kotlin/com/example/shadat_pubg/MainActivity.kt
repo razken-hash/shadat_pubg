1e663a32398643e9ef5816a1e560c3dd20028150
razken-hash <ka_kenniche@esi.dz>
2024-08-10 09:48:48 +0100

    ShadatPUBG Project App Init

---
+++ android/app/src/main/kotlin/com/example/shadat_pubg/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shadat_pubg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ShadatPubg());
+}
+
+class ShadatPubg extends StatelessWidget {
+  const ShadatPubg({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
