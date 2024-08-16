dc6022877c51fb265d3186171c662113fa5ba521
razken-hash <ka_kenniche@esi.dz>
2024-08-16 17:46:04 +0100

    ProfileScreen Redesigned With .7 Opcaity

--- lib/views/screens/profile/profile_screen.dart
+++ lib/views/screens/profile/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shadat_pubg/views/config/assets_manager.dart';
 import 'package:shadat_pubg/views/themes/colors.dart';
 import 'package:shadat_pubg/views/widgets/pubg_scaffold.dart';
 
@@ -9,15 +10,70 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return PubgScaffold(
       backgroundImage: "pubg_3",
-      content: const Center(
-        child: Text(
-          "Profile Screen",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: PubgColors.whiteColor,
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-          ),
+      content: Container(
+        height: 500,
+        width: double.infinity,
+        margin: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: PubgColors.whiteColor.withOpacity(.7),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          children: [
+            CircleAvatar(
+              backgroundColor: PubgColors.primaryColor,
+              radius: 64,
+              child: CircleAvatar(
+                radius: 60,
+                foregroundImage: AssetImage(AssetsManager.getImage("person")),
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "ABDERRAZAK KENNICHE",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: PubgColors.blackColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "ka_kenniche@esi.dz",
+              style: TextStyle(
+                color: PubgColors.blackColor,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Divider(
+              color: PubgColors.primaryColor,
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "عدد الشدات لديك: 101 شدة",
+              style: TextStyle(
+                color: PubgColors.blackColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "تسجيل الخروج",
+                style: TextStyle(
+                  color: PubgColors.whiteColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
