40d9bfbe16f30986b58c5d52e2d5c63ec35ec01d
razken-hash <ka_kenniche@esi.dz>
2024-08-16 17:47:06 +0100

    InviteFriendsScreen Redesigned + A Responsivity Problem While Typing In TextField

--- lib/views/screens/invite_friends/invite_friends_screen.dart
+++ lib/views/screens/invite_friends/invite_friends_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shadat_pubg/views/config/assets_manager.dart';
 import 'package:shadat_pubg/views/themes/colors.dart';
 import 'package:shadat_pubg/views/widgets/pubg_scaffold.dart';
 
@@ -8,16 +11,117 @@ class InviteFriendsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PubgScaffold(
-      backgroundImage: "pubg_1",
-      content: const Center(
-        child: Text(
-          "Invite Friends Screen",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: PubgColors.whiteColor,
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-          ),
+      backgroundImage: "pubg_2",
+      content: Container(
+        height: 600,
+        width: double.infinity,
+        margin: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: PubgColors.whiteColor.withOpacity(.7),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          children: [
+            const Text(
+              "أدخل رمز إحالة الصديق",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: PubgColors.blackColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const FractionallySizedBox(
+              widthFactor: .6,
+              child: TextField(
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "تأكيــد",
+                style: TextStyle(
+                  color: PubgColors.whiteColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0),
+              child: Divider(color: PubgColors.primaryColor),
+            ),
+            const Text(
+              "قم بدعوة الأصدقاء والعائلة",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: PubgColors.blackColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 15),
+            FractionallySizedBox(
+              widthFactor: .6,
+              child: TextField(
+                controller: TextEditingController(
+                  text: "123456",
+                ),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  prefixIcon: SvgPicture.asset(
+                    AssetsManager.getVector(
+                      "document",
+                    ),
+                  ),
+                  suffixIcon: SvgPicture.asset(
+                    AssetsManager.getVector(
+                      "document",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "مشاركة الكود",
+                style: TextStyle(
+                  color: PubgColors.whiteColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0),
+              child: Divider(color: PubgColors.primaryColor),
+            ),
+            const Text(
+              "عدد الدعوات التي قمت بإرسالها\n 0",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: PubgColors.blackColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            const Text(
+              "يمكنك الحصول على 10 شدات عن طريق إحالة صديق ويتمكن الصديق من الحصول على 3 لفات مجانية",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: PubgColors.blackColor,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
